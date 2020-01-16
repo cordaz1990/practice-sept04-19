4022ffbe20574d48896e7377d686723a041a7092
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:38:24 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,17 @@
-print('hello')
-print('world')
-print('hello')
+#polymorphism
+
+def histogram(s):
+    d = dict()
+    for c in s:
+      if c not in d:
+          d[c] = 1
+      else:
+        d[c] = d[c] + 1
+        
+    return d
+  
+  
+  t = ['spam', 'egg', 'spam', 'spam', 'spam', 'bacon', 'spam']
+  histograms(t)
+  {'bacon' : 1, 'egg':1, 'spam': 4}
+  
